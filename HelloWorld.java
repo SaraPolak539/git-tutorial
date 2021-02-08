f88291877075971894ac94106e54eff7dad6f736
SaraPolak539 <sara@realit.org>
2021-02-08 12:36:43 +0200

    adding file2.txt and HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//file2  into tutoiral git
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-tutorial
 first comment in read.me
+i added file1.txt - general file
+file2.txt - general file
+HelloWorld.java - my first program
